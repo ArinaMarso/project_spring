6f8df490ab2c619697b7298e51de4524e4cc4f07
ArinaMarso <arinovaa54@gmail.com>
2026-05-06 02:05:11 +0300

    Add files via upload

---
+++ utils/EnumBindingSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace StoreG5G11.modelViews;
+
+public class EnumBindingSourceExtension : MarkupExtension
+{
+    public Type EnumType { get; private set; }
+
+    public EnumBindingSourceExtension(Type enumType)
+    {
+        if (enumType is null || !enumType.IsEnum)
+            throw new Exception("EnumType must not be null and of type Enum");
+
+        EnumType = enumType;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Enum.GetValues(EnumType);
+    }
+}
---
+++ utils/EnumBindingSourceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace StoreG5G11.views;
+
+public class EnumBindingSourceConverter : IValueConverter
+{
+    public static EnumBindingSourceConverter Instance { get; } = new EnumBindingSourceConverter();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Type enumType && enumType.IsEnum)
+        {
+            return Enum.GetValues(enumType);
+        }
+        return Array.Empty<object>();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
