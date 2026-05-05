225dfe9cea98375cadf9b53b7cba431a0a3001f8
ArinaMarso <arinovaa54@gmail.com>
2026-05-06 02:06:37 +0300

    Add files via upload

---
+++ views/EmployeesView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace StoreG5G11.views;
+
+public partial class EmployeesView : Window
+{
+    public EmployeesView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ views/SalesView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace StoreG5G11.views;
+
+public partial class SalesView : Window
+{
+    public SalesView()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
