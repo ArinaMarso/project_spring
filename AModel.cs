669a7e410029b744289087576465a11340e215a1
ArinaMarso <arinovaa54@gmail.com>
2026-05-06 01:50:32 +0300

    Add files via upload

---
+++ AModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace StoreG5G11.models.ef;
+
+public abstract class AModel : INotifyPropertyChanged, IValidatableObject
+{
+    [Key] public int Id { get; set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public abstract IEnumerable<ValidationResult> Validate(ValidationContext validationContext);
+
+
+    protected bool SetField<T>(ref T field, T value, Action<string,T>[]? validators = null,
+        [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+        if (validators != null)
+        {
+            foreach (var validator in validators)
+            {
+                validator(propertyName!, value);
+            }
+        }
+
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    public abstract AModel GetCopy();
+    public abstract void CopyFrom(AModel other);
+    public abstract bool Equals(AModel other);
+}
