eac1658efdf7de86df6721e40e74f887d923a7a2
ArinaMarso <arinovaa54@gmail.com>
2026-05-06 02:00:25 +0300

    Add files via upload

---
+++ models/entities/InstrumentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreG5G11.src.models.ef.entities
+{
+    public enum InstrumentType
+    {
+        String = 1,        
+        Wind = 2,           
+        Percussion = 3,     
+        Keyboard = 4,       
+        Electronic = 5      
+    }
+}
